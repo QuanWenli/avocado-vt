39f175246f5217d513dc28b52dacf8c85c7454b8
Wenli Quan <wquan@redhat.com>
2018-08-15 11:16:13 +0800

    There is no six module, no need to import it in wait_for_quit
    Signed-off-by: Wenli Quan <wquan@redhat.com

--- shared/scripts/wait_for_quit.py
+++ shared/scripts/wait_for_quit.py
@@ -1,8 +1,10 @@
-from six.moves import input
-
 
 while True:
-    in_put = input("")
+    try:
+        _input = raw_input
+    except NameError:
+        _input = input
+    in_put = _input("")
     if in_put in ['exit', 'quit']:
         print("Bye")
         break
